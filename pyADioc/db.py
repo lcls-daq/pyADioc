87f8a3eef30ebe3b7d4501ae4cbffb8f6ac14d4c
Daniel Damiani <ddamiani@slac.stanford.edu>
2018-10-24 15:38:05 -0700

    improved db setup

--- pyADioc/db.py
+++ pyADioc/db.py
@@ -1,10 +1,131 @@
+import numpy as np
+
+CONFIG = {
+    'Opal1k': {
+        'model': "Adimec",
+        'manufacturer': "1000m/CL",
+        'shape': (1024, 1024, 12),
+        'size': 2
+    },
+    'Pulnix' : {
+        'model': 'TM6740CL',
+        'manufacturer': 'PULNIX',
+        'shape': (480, 640, 10),
+        'size': 2
+    },
+    'Visar' : {
+        'model': 'C7700',
+        'manufacturer': 'Hamamatsu',
+        'shape': (1024, 1344, 16),
+        'size': 2,
+        'extra': {
+            'ScaleX_RBV' : {
+                'type': 'float',
+                'value': 1e-7,
+                'autosave' : True,
+            },
+            'ScaleY_RBV' : {
+                'type': 'float',
+                'value': 1.0,
+                'autosave' : True,
+            },
+            'TimeRange_RBV' : {
+                'type': 'enum',
+                'enums': [
+                    '0.5 ns',
+                    '1 ns',
+                    '5 ns',
+                    '10 ns',
+                    '20 ns',
+                    '50 ns',
+                    '100 ns',
+                    '200 ns',
+                    '500 ns',
+                    '1 us',
+                    '2 us',
+                    '5 us',
+                    '10 us',
+                    '20 us',
+                    '50 us',
+                    '100 us',
+                    '200 us',
+                    '500 us',
+                    '1 ms',
+                ],
+                'value': 0,
+                'autosave' : True,
+            },
+            'TriggerMode_RBV' : {
+                'type': 'enum',
+                'enums': ['Focus', 'Operate'],
+                'value': 0,
+                'autosave' : True,
+            },
+            'GateMode_RBV' : {
+                'type': 'enum',
+                'enums': ['Normal', 'Gate', 'Open Fixed'],
+                'value': 0,
+                'autosave' : True,
+            },
+            'Shutter_RBV' : {
+                'type': 'enum',
+                'enums': ['Closed', 'Open'],
+                'value': 0,
+                'autosave' : True,
+            },
+            'ImageMode_RBV' : {
+                'type': 'enum',
+                'enums': ['Single', 'Continuous'],
+                'value': 0,
+                'autosave' : True,
+            },
+            'FocusTimeOver_RBV' : {
+                'type': 'int',
+                'value': 5,
+                'autosave' : True,
+            },
+            'ScalingFilePath' : {
+                'type': 'char',
+                'count': 10000,
+                'value': '/reg/neh/home/joaoprod/visar/mec/visar/current/VISAR1.txt',
+                'autosave' : True,
+            },
+        },
+    },
+}
+
+def get_max_array_size(camtype):
+    size = 0x10000
+    if camtype in CONFIG:
+        height, width, _ = CONFIG[camtype]['shape']
+        size += height * width * CONFIG[camtype]['size']
+    else:
+        size = 40000000
+
+    return size
+
+def get_dtype(camtype):
+    if camtype in CONFIG:
+        size = CONFIG[camtype]['size']
+        if size <= 8:
+            return np.uint8
+        elif size <= 16:
+            return np.uint16
+        else:
+            return np.uint32
+    else:
+        return np.uint16
+
 def init(camtype):
-    if camtype == 'Opal1k':
-        pvdb = init_base("Adimec", "1000m/CL", 1024, 1024, 12)
-    elif camtype == 'Pulnix':
-        pvdb = init_base("Adimec", "1000m/CL", 480, 640, 10)
-    elif camtype == 'Visar':
-        pvdb = init_base("Hamamatsu", "CC7700", 1024, 1344, 16)
+    if camtype in CONFIG:
+        pvdb = init_base(
+            CONFIG[camtype]['manufacturer'],
+            CONFIG[camtype]['model'],
+            *CONFIG[camtype]['shape']
+        )
+        extras = CONFIG[camtype].get('extra', None)
+        if extras is not None:
+            pvdb.update(extras)
     else:
         pvdb = None
     
